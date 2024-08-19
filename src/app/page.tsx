31c7c627fd5b3e77222448502b2e172947eef20c
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-08-19 18:05:10 +0300

    install next ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
+import { Button } from '@nextui-org/react'
 export default function Home() {
   return (
     <main >
       <h1>init of web talk</h1>
+      <Button>click</Button>
     </main>
   )
 }
