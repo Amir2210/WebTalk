d858805a1895f77ebb214ef77d30ccc3a01f2831
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-08-20 16:26:05 +0300

    Creating the routing structure

---
+++ src/app/topics/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TopicShowPage() {
+  return (
+    <div>TopicShowPage</div>
+  )
+}
+
+export default TopicShowPage
---
+++ src/app/topics/[slug]/posts/[postid]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostShowPage() {
+  return (
+    <div>PostShowPage</div>
+  )
+}
+
+export default PostShowPage
