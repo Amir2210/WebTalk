ab447233545082045058bcb0d4267f9650c5a67f
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-08-25 10:28:54 +0300

    Render topic slug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import TopicCreateForm from '@/components/topics/TopicCreateForm'
+import TopicList from '@/components/topics/TopicList'
+import { Divider } from '@nextui-org/react'
 export default async function Home() {
   return (
     <div className='grid grid-cols-4 gap-4 p-4'>
       <div className='col-span-3'>
         <h1 className='text-xl m-2'>Top Posts</h1>
       </div>
-      <div>
+      <div className='border shadow-sm py-3 px-2'>
         <TopicCreateForm />
+        <Divider className='my-2'></Divider>
+        <h3 className='text-lg my-2'>Topics</h3>
+        <TopicList />
       </div>
     </div>
   )
